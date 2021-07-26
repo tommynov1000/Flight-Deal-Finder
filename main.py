d613e642b20e29c702d602139bea11d0f601d6e0
tommynov1000 <tommynov1000@GMAIL.com>
2021-07-26 18:05:24 -0400

    Removing old code.

--- main.py
+++ main.py
@@ -17,39 +17,24 @@ to_time = dt.datetime.now() + dt.timedelta(days=(6 * 30))
 flight_search = FlightSearch()
 notification_manager = NotificationManager
 
-# for city in data_manager.flights:
-#     if city["IATA Code"] is None:
-#         city["IATA Code"] = flight_search.get_IATA_code(city["City"])
-#
-#     print(f"Searching for prices to {city['IATA Code']}")
-#
-#     flight = flight_search.find_cheapest_flight(
-#         origin=ORIGIN_CITY_IATA,
-#         destination=city["IATA Code"],
-#         from_time=from_time,
-#         to_time=to_time
-#     )
-#
-#     if flight != f"No flights found to {city['IATA Code']}":
-#         print(f"Found flight for {flight.total_price}")
-#         if flight.total_price <= city["Lowest Price"]:
-#             print("Found cheap price! Sending email!\n")
-#             for user in data_manager.users:
-#                 notification_manager.send_msg(flight, user)
-#     else:
-#         print(flight + "\n")
+for city in data_manager.flights:
+    if city["IATA Code"] is None:
+        city["IATA Code"] = flight_search.get_IATA_code(city["City"])
 
+    print(f"Searching for prices to {city['IATA Code']}")
 
-flight = flight_search.find_cheapest_flight(
-    origin="ALB",
-    destination="RDU",
-    from_time=from_time,
-    to_time=to_time
-)
+    flight = flight_search.find_cheapest_flight(
+        origin=ORIGIN_CITY_IATA,
+        destination=city["IATA Code"],
+        from_time=from_time,
+        to_time=to_time
+    )
 
-if flight != f"No flights found to RDU":
-    if flight.total_price <= 500:
-        for user in data_manager.users:
-            notification_manager.send_msg(flight, user)
-else:
-    print(flight)
+    if flight != f"No flights found to {city['IATA Code']}":
+        print(f"Found flight for {flight.total_price}")
+        if flight.total_price <= city["Lowest Price"]:
+            print("Found cheap price! Sending email!\n")
+            for user in data_manager.users:
+                notification_manager.send_msg(flight, user)
+    else:
+        print(flight + "\n")
